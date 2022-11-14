387a053b3c59b3a85c64fda390312e30cd120856
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-14 17:32:11 +0300

    add banner to landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,4 +1,5 @@
 import "./landing.css";
+import ban from "../images/Full-Stack.svg";
 export default function Landing() {
   function Header() {
     return <header></header>;
@@ -7,14 +8,29 @@ export default function Landing() {
   function Info() {
     return (
       <div className="item-info">
-        <span></span>
-        <span></span>
+        <span className="intro">
+          <span>
+            Hey There,
+            <br />
+            I'm Mokua
+          </span>
+          <button>Developer</button>
+        </span>
+        <span>
+          A full-stack developer with over one year of industrial experience in
+          various domains. Worked on multiple projects in Finance, Education and
+          Medicine fields.
+        </span>
       </div>
     );
   }
 
   function Banner() {
-    return <div className="banner">Banner</div>;
+    return (
+      <div className="banner">
+        <img src={ban} alt="banner" />
+      </div>
+    );
   }
   return (
     <main>
