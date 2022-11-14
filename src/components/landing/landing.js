38a193db730485253094c2a002e15b5d50fd7e94
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-14 16:44:25 +0300

    create componets for landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,3 +1,21 @@
+import "./landing.css";
 export default function Landing() {
-  return <main>Landing</main>;
+  function Header() {
+    return <header></header>;
+  }
+
+  function Info() {
+    return <div className="item-info"></div>;
+  }
+
+  function Banner() {
+    return <div className="banner">Banner</div>;
+  }
+  return (
+    <main>
+      <Header />
+      <Banner />
+      <Info />
+    </main>
+  );
 }
