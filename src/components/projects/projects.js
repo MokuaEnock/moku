d08a8ba3ac988045775e3e0ae8f3b6d40a1af538
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-14 19:31:45 +0300

    style projects header

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,7 +1,18 @@
 import "./projects.css";
+import Search from "../images/search.svg";
 export default function Projects() {
   function Header() {
-    return <div>Header</div>;
+    return (
+      <div className="projects-header">
+        <span>Latest</span>
+        <form>
+          <button type="submit">
+            <img src={Search} alt="search" />
+          </button>
+          <input type="text" placeholder="Search" />
+        </form>
+      </div>
+    );
   }
   function Body() {
     return <div>Body</div>;
