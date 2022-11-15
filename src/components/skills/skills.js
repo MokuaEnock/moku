44f937065ba5bbb876aa7b54f873f483e1d63fda
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-15 16:19:30 +0300

    create skills card

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,3 +1,43 @@
+import "./skill.css";
+
 export default function Skills() {
-  return <section>SKills</section>;
+  return (
+    <section id="skill">
+      <header id="skills-head">Skills</header>
+
+      <div id="skills-body">
+        <div className="skills-card">
+          <span className="skills-card-heading">Languages</span>
+          <div className="skills-card-list">
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+          </div>
+        </div>
+
+        <div className="skills-card">
+          <span className="skills-card-heading">Languages</span>
+          <div className="skills-card-list">
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+          </div>
+        </div>
+
+        <div className="skills-card">
+          <span className="skills-card-heading">Languages</span>
+          <div className="skills-card-list">
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+            <span className="skills-card-item">React.Js</span>
+          </div>
+        </div>
+      </div>
+
+      {/* <button id="view">View All</button> */}
+    </section>
+  );
 }
