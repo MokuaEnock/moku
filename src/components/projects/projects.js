b0d52ee53a9c08a327def8d01dba8e516cf16567
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-15 20:37:31 +0300

    create project card

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,5 +1,8 @@
 import "./projects.css";
 import Search from "../images/search.svg";
+import Vector from "../images/Vector.svg";
+import Github from "../images/Vector (1).svg";
+
 export default function Projects() {
   function Header() {
     return (
@@ -15,7 +18,41 @@ export default function Projects() {
     );
   }
   function Body() {
-    return <div id="projects-body">Body</div>;
+    return (
+      <div id="projects-body">
+        <div className="project-card" id="card1">
+          <span className="project-card-image">
+            <img src="" />
+          </span>
+          <span className="project-card-title">Machine Learning</span>
+          <span className="project-card-details">
+            <button>Python </button>
+            <button>SciKit</button>
+            <button>Classifier</button>
+          </span>
+          <span className="project-card-info">
+            Sentiment analysis modelling.
+          </span>
+          <span className="project-card-buttons">
+            <button>
+              Live <img src={Vector} alt="" />
+            </button>
+            <button>
+              GitHub <img src={Github} alt="github" />
+            </button>
+          </span>
+        </div>
+        <div className="project-card" id="card2"></div>
+        <div className="project-card" id="card3"></div>
+      </div>
+    );
+  }
+
+  function Section() {
+    return <div></div>;
+  }
+  function Section1() {
+    return <div></div>;
   }
 
   function Footer() {
@@ -25,6 +62,8 @@ export default function Projects() {
     <section id="projects">
       <Header />
       <Body />
+      <Section />
+      <Section1 />
       <Footer />
     </section>
   );
