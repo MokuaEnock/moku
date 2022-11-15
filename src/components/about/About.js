0cefce8bcb446125e60ba831a0e7b9175afbe4e1
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-15 13:33:11 +0300

    create and style about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,3 +1,15 @@
+import "./about.css";
 export default function About() {
-  return <section>About</section>;
+  return (
+    <section id="about">
+      <span id="title">About</span>
+      <span id="body">
+        I am a javascript and ruby developer based in Nairobi Kenya. I can
+        develop fulllstack web applications from scratch and develop modern
+        user-friendly experiences. I am also data oriented with a strong
+        interest in machine learning and intergrating machine learning
+        algorithms into applications and getting value from the algorithms.
+      </span>
+    </section>
+  );
 }
