b218e266cd937f84352c3f059e41b26a7f052ff4
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-15 14:30:35 +0300

    create skeleton for education section

--- src/components/education/education.js
+++ src/components/education/education.js
@@ -1,3 +1,21 @@
+import "./education.css";
 export default function Education() {
-  return <section>Education and experience</section>;
+  return (
+    <section id="edu">
+      <header id="edu-header">Experience</header>
+      <div id="exp">
+        <span className="title">Experience</span>
+        <div></div>
+      </div>
+      <div id="educ">
+        <span className="title">Education</span>
+        <div></div>
+      </div>
+
+      <div id="cert">
+        <span className="title">Certifications</span>
+        <div></div>
+      </div>
+    </section>
+  );
 }
