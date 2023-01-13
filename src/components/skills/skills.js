f3213facc415fbab8ee30a24579e2ccd3ec18c79
Enock <enock.mokua@student.moringaschool.com>
2023-01-13 03:31:39 +0300

    fetch data asynchronously for useeffect

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,10 +1,26 @@
 import "./skill.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Skills() {
   let [data, setData] = useState(null);
   let [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    let fetchData = async () => {
+      setIsLoading(true);
+      try {
+        let response = await fetch("");
+        let data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <section id="skill">
       <header id="skills-head">Skills</header>
