c29d75210dbe1ff0c2a14e28ace3fcbc68b6a90f
Enock <enock.mokua@student.moringaschool.com>
2023-01-15 07:20:48 +0300

    create edus data file

---
+++ src/components/education/edu.js
@@ -0,0 +1,26 @@
+let edus = [
+  {
+    id: 1,
+    institution: "Mount Kenya University",
+    type: "Degree",
+    field: "Education",
+    major: "Mathematics Physics",
+    start_date: "01/05/2019",
+    end_date: "29/07/2022",
+    issue_date: "29/07/2022",
+    institution_link: "https://www.mku.ac.ke/",
+  },
+  {
+    id: 2,
+    institution: "Moringa School",
+    type: "Certificate",
+    field: "Software engineering",
+    major: "Full stack web development",
+    start_date: "01/05/2019",
+    end_date: "29/07/2022",
+    issue_date: "29/07/2022",
+    institution_link: "https://moringaschool.com/",
+  },
+];
+
+export default edus;
