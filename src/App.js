cd9edae1c8b1dc15e08a55496fd2cbacc645f286
Enock <enock.mokua@student.moringaschool.com>
2023-01-20 22:53:52 +0300

    configure routes from react router dom

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Education from "./components/education/education";
 import Landing from "./components/landing/landing";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Landing />
-      <Projects />
-      <Skills />
-      <Education />
-      <About />
-      <Contact />
-    </>
+    <Router>
+      <Routes>
+        {/* <Landing />
+        <Projects />
+        <Skills />
+        <Education />
+        <About />
+        <Contact /> */}
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
