20b0394745ae28daaf139451bc81767574d081b3
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 13:31:31 +0300

    create seach form for skill header

--- src/components/skills/SkillHeader.js
+++ src/components/skills/SkillHeader.js
@@ -1,3 +1,14 @@
+import {Link} from "react-router-dom"
+
+
 export default function SkillHeader(){
-    return<header>Skill Header</header>
+    return<header>
+        <Link to="/">Home</Link>
+
+        <form>
+            <input type="text" placeholder="Search Skill"/>
+            <button type="submit">Search</button>
+        </form>
+        
+    </header>
 }
