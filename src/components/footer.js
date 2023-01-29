29e0083ac99d0736e511747d0bd0163beddc7db5
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 17:41:14 +0300

    style footer header

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,13 @@ export default function Footer() {
   return <footer>
     <div className="footer-header">
       <p>Subscribe to my weekly newsletter</p>
-      <form>
+      <form className="footer-form">
         <input type="text" placeholder="Enter your Email"/>
         <button type="submit">Subscribe</button>
       </form>
     </div>
     <div className="footer-main">
-      
+
     </div>
     <p>Developed and maintained by Enock Mokua</p>
   </footer>;
