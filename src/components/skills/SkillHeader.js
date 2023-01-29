5d330922211be8fc63368c591fb754aad4627e02
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 13:54:04 +0300

    style serach skill form

--- src/components/skills/SkillHeader.js
+++ src/components/skills/SkillHeader.js
@@ -5,7 +5,7 @@ export default function SkillHeader() {
   return (
     <header id="skill-header">
       <Link to="/">Home</Link>
-
+dds
       <form id="skill-search">
         <input type="text" placeholder="Search Skill" />
         <button type="submit">Search</button>
