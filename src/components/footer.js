484d0638622dd80026825f4a2d3499f928d17e7e
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 18:02:17 +0300

    create structure of footer main

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,27 @@
-export default function Footer() {
-  return <footer>
-    <div className="footer-header">
-      <p>Subscribe to my weekly newsletter</p>
-      <form className="footer-form">
-        <input type="text" placeholder="Enter your Email"/>
-        <button type="submit">Subscribe</button>
-      </form>
-    </div>
-    <div className="footer-main">
+import { Link } from "react-router-dom";
 
-    </div>
-    <p>Developed and maintained by Enock Mokua</p>
-  </footer>;
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-header">
+        <p>Subscribe to my weekly newsletter</p>
+        <form className="footer-form">
+          <input type="text" placeholder="Enter your Email" />
+          <button type="submit">Subscribe</button>
+        </form>
+      </div>
+      <div className="footer-main">
+      <span></span>
+
+        <span>
+          <Link to="/projects">Projects</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/skills">Skills</Link>
+          <Link to="/experience">Experience</Link>
+        </span>
+        <span></span>
+      </div>
+      <p>Developed and maintained by Enock Mokua</p>
+    </footer>
+  );
 }
