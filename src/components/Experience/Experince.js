5f5a446abe958f0be2cb602bea960d11f09fa928
Enock <enock.mokua@student.moringaschool.com>
2023-01-30 14:48:18 +0300

    create basic structurw and default styling for experinces section

--- src/components/Experience/Experince.js
+++ src/components/Experience/Experince.js
@@ -2,10 +2,21 @@ import ExperinceHeader from "./ExperienceHeader";
 import Education from "./Education";
 import Certification from "./Certiification";
 import Events from "./Events";
-import "./exp.css"
+import "./exp.css";
+
 export default function Experience() {
   function Exp() {
-    return <section>Experinces</section>;
+    return (
+      <section id="experience-section">
+        <div className="experience-header-display">
+          <span></span>
+          <span>Experience</span>
+          <span></span>
+        </div>
+
+        <div className="experience-card"></div>
+      </section>
+    );
   }
 
   return (
