519876d6667a04f1357c373926de482864a5bd59
Enock <enock.mokua@student.moringaschool.com>
2023-01-30 14:28:57 +0300

    create experinces.css file

--- src/components/Experience/Experince.js
+++ src/components/Experience/Experince.js
@@ -2,7 +2,7 @@ import ExperinceHeader from "./ExperienceHeader";
 import Education from "./Education";
 import Certification from "./Certiification";
 import Events from "./Events";
-
+import "./exp.css"
 export default function Experience() {
   function Exp() {
     return <section>Experinces</section>;
