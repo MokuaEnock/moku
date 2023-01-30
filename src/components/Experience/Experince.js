e047fbb6f24eb36724229ba9b41d34dc3387637b
Enock <enock.mokua@student.moringaschool.com>
2023-01-30 15:45:27 +0300

    create the basic structure of experince card

--- src/components/Experience/Experince.js
+++ src/components/Experience/Experince.js
@@ -14,7 +14,27 @@ export default function Experience() {
           <span></span>
         </div>
 
-        <div className="experience-card"></div>
+        <div className="experience-card">
+          <div className="experience-card-1">
+            <h3>Alphabet Inc.</h3>
+
+            <span className="experince-card-info">
+              <p>Machine Learning Engineer</p>
+              <p>Start: 01/06/2022</p>
+              <p>End: 31/12/2022</p>
+              <p>Duration: 6 months</p>
+            </span>
+
+            <span className="experience-card-button">
+              <button>View Site</button>
+            </span>
+          </div>
+
+          <div className="experience-card-1">
+            <h4>Roles</h4>
+            <p>Built a reccomendation engine for alphabet Inc.</p>
+          </div>
+        </div>
       </section>
     );
   }
